37093cc308183b9f46ba8f8dfdce28e98d1a3122
yuonoda <yuonoda95@gmail.com>
2021-05-27 22:45:46 +0900

    Echoを導入

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/yuonoda/bookspace
 go 1.15
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 	gorm.io/driver/mysql v1.1.0
 	gorm.io/gorm v1.21.10
 )
--- main.go
+++ main.go
@@ -1,31 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"log"
+	"net/http"
 )
 
 func main() {
-	dsn := "localuser:localpass@tcp(127.0.0.1:3306)/localdb?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", welcome())
+
+	err := e.Start(":3000")
 	if err != nil {
-		panic("failed to connect database")
+		log.Fatalln(err)
 	}
-
-	// Migrate the schema
-	db.Migrator().DropTable(&User{}, &Book{})
-	db.AutoMigrate(&User{}, &Book{})
-
-	user := &User{
-		Name: "小野田　祐",
-	}
-	db.Create(user)
-	fmt.Printf("user:%v\n", user)
-
-	b := &Book{
-		OwnerId: user.UserId,
-		Title:   "サンプルの本1",
-	}
-	db.Create(&b)
 }
+func welcome() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome to BookSpace API")
+	}
+}
+
+//
+//func main() {
+//	dsn := "localuser:localpass@tcp(127.0.0.1:3306)/localdb?charset=utf8mb4&parseTime=True&loc=Local"
+//	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+//	if err != nil {
+//		panic("failed to connect database")
+//	}
+//
+//	// Migrate the schema
+//	db.Migrator().DropTable(&User{}, &Book{})
+//	db.AutoMigrate(&User{}, &Book{})
+//
+//	user := &User{
+//		Name: "小野田　祐",
+//	}
+//	db.Create(user)
+//	fmt.Printf("user:%v\n", user)
+//
+//	b := &Book{
+//		OwnerId: user.UserId,
+//		Title:   "サンプルの本1",
+//	}
+//	db.Create(&b)
+//}
