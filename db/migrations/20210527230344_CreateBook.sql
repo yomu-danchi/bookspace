68c90536fb2e5b3001aae33af30bbc36aa04d41a
yuonoda <yuonoda95@gmail.com>
2021-05-31 15:11:55 +0900

    テーブル名を修正

--- db/migrations/20210527230344_CreateBook.sql
+++ db/migrations/20210527230344_CreateBook.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-CREATE TABLE Books (
+CREATE TABLE books (
     book_id SERIAL,
     user_id BIGINT UNSIGNED NOT NULL,
     isbn13 INTEGER NOT NULL,
--- db/migrations/20210530225902_CreateUser.sql
+++ db/migrations/20210530225902_CreateUser.sql
@@ -1,15 +1,15 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-CREATE TABLE Users
+CREATE TABLE users
 (
     user_id           SERIAL,
     user_name         VARCHAR(255) NOT NULL,
     user_display_name VARCHAR(255) NOT NULL
 );
-ALTER TABLE Books
+ALTER TABLE books
     ADD FOREIGN KEY (user_id)
-    REFERENCES Users(user_id)
+    REFERENCES users(user_id)
     ON DELETE CASCADE;
 
 -- +goose Down
