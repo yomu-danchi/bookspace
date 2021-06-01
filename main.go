9df47fab0cbc36da2df0dd100410eb23947ed661
yuonoda <yuonoda95@gmail.com>
2021-06-01 15:12:28 +0900

    古いコードを削除

--- main.go
+++ main.go
@@ -53,28 +53,3 @@ func welcome() echo.HandlerFunc {
 		return c.String(http.StatusOK, "Welcome to BookSpace API")
 	}
 }
-
-//
-//func main() {
-//	dsn := "localuser:localpass@tcp(127.0.0.1:3306)/localdb?charset=utf8mb4&parseTime=True&loc=Local"
-//	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-//	if err != nil {
-//		panic("failed to connect database")
-//	}
-//
-//	// Migrate the schema
-//	db.Migrator().DropTable(&User{}, &Book{})
-//	db.AutoMigrate(&User{}, &Book{})
-//
-//	user := &User{
-//		Name: "小野田　祐",
-//	}
-//	db.Create(user)
-//	fmt.Printf("user:%v\n", user)
-//
-//	b := &Book{
-//		OwnerId: user.UserId,
-//		Title:   "サンプルの本1",
-//	}
-//	db.Create(&b)
-//}
