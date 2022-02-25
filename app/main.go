eeff2d0037761978f4707f579262b7235edd0f2d
yuonoda <yuonoda95@gmail.com>
2022-02-25 23:28:15 +0900

    feat: firestoreにデータを保存できるようにした

--- app/main.go
+++ app/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"fmt"
+	"google.golang.org/api/iterator"
+	"os"
+
 	//"github.com/99designs/gqlgen/graphql/handler"
 	//"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/labstack/echo"
@@ -14,11 +20,43 @@ import (
 )
 
 func main() {
-	//// TODO 環境変数にする
-	//dsn := "localuser:localpass@tcp(db:3306)/localdb?charset=utf8mb4&parseTime=True&loc=Local"
-	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	ctx := context.Background()
+	host := os.Getenv("FIRESTORE_EMULATOR_HOST")
+	log.Printf("host: %+v", host)
+	store, err := firestore.NewClient(ctx, "test-project-id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("store: %+v", store)
+
+	doc, _, err := store.Collection("users").Add(ctx, map[string]interface{}{
+		"name": "hello",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("doc: %+v", doc)
+
+	//// Use the application default credentials
+	//ctx := context.Background()
+	//conf := &firebase.Config{ProjectID: "test-project-id"}
+	//app, err := firebase.NewApp(ctx, conf)
 	//if err != nil {
-	//	panic("failed to connect database")
+	//    log.Fatalln(err)
+	//}
+	//client, err := app.Firestore(ctx)
+	//if err != nil {
+	//    log.Fatalln(err)
+	//}
+	//defer client.Close()
+	//
+	//_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+	//    "first": "Ada",
+	//    "last":  "Lovelace",
+	//    "born":  1815,
+	//})
+	//if err != nil {
+	//    log.Fatalf("Failed adding alovelace: %v", err)
 	//}
 
 	e := echo.New()
@@ -49,6 +87,23 @@ func main() {
 }
 func welcome() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return c.String(http.StatusOK, "Welcome to BookSpace API!")
+		ctx := context.Background()
+		store, err := firestore.NewClient(ctx, "test-project-id")
+		if err != nil {
+			log.Fatal(err)
+		}
+		iter := store.Collection("users").Documents(ctx)
+		for {
+			doc, err := iter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Failed to iterate: %v", err)
+			}
+			fmt.Println(doc.Data())
+		}
+		log.Printf("iter:%+v", iter)
+		return c.String(http.StatusOK, "Welcome to BookSpace API! ")
 	}
 }
