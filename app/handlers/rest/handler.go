a83e85319b9cf44a7ca2f8a2985691e8a10bc48b
yuonoda <yuonoda95@gmail.com>
2022-02-28 21:57:20 +0900

    feat: 本を登録するエンドポイントをつくった

--- app/handlers/rest/handler.go
+++ app/handlers/rest/handler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-chi/render"
 	"github.com/yuonoda/bookspace/app/usecase"
 	"github.com/yuonoda/bookspace/app/usecase/dto"
+	"golang.org/x/xerrors"
 	"log"
 	"net/http"
 )
@@ -53,12 +54,25 @@ func (h handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 	user, err := h.usecase.CreateUser(ctx, dtoUser)
 	if err != nil {
-		log.Print(fmt.Errorf("failed to get users :%w", err))
+		log.Print(xerrors.Errorf(":%w", err))
 		render.Status(r, 404)
 	}
 	render.JSON(w, r, user)
 }
 
 func (h handler) RegisterBook(w http.ResponseWriter, r *http.Request) {
-	return
+	ctx := r.Context()
+	var dtoBook dto.Book
+	// TODO JSON が間違っていたときのバリデーション
+	err := json.NewDecoder(r.Body).Decode(&dtoBook)
+	if err != nil {
+		render.Status(r, 400)
+		return
+	}
+	user, err := h.usecase.RegisterBook(ctx, dtoBook)
+	if err != nil {
+		log.Print(xerrors.Errorf(":%w", err))
+		render.Status(r, 400)
+	}
+	render.JSON(w, r, user)
 }
